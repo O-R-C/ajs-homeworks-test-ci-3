a48b320891fb2334b9ab761c2c9df6056da3899c
Andrey Semenov <semenov.orc@gmail.com>
2024-01-16 15:11:31 +0300

    chanje http.js

--- src/js/http.js
+++ src/js/http.js
@@ -5,3 +5,8 @@ export function httpGet(url) {
 export function httpPost(url) {
   throw new Error(url);
 }
+
+// Демо-реализация функции fetchData (модуль http):
+export default function fetchData(url) {
+  throw new Error("Mock this!", url);
+}
